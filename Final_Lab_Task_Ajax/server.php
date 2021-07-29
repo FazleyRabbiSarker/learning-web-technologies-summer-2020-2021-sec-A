489311d50dc201e2fb34b29739a43913b0e5780c
Fazley Rabbi <sarkerrabbi250@gmail.com>
2021-07-29 23:27:38 +0600

    AJAX_LAB-TASK

---
+++ Final_Lab_Task_Ajax/server.php
@@ -0,0 +1,14 @@
+
+<?php
+	require_once("userModel.php");
+
+	$email = $_GET['email'];
+
+	if($email == null){
+		echo 'Enter email...';
+	}else{
+		addEmail($email);
+		echo "Subscribed: ".$email;
+	}
+
+?>
