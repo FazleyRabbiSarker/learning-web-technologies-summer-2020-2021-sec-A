9d48c4c48883cdedf77eebc9545dca089463bb92
Fazley Rabbi <sarkerrabbi250@gmail.com>
2021-08-01 11:02:04 +0600

    MINI_PROJECT

---
+++ FINAL_MINI_PROJECT/controller/Edit Profile.php
@@ -0,0 +1,25 @@
+<?php
+
+	session_start();
+
+	if(isset($_POST['submit']))
+    { 
+
+		$user = $_SESSION['current_user'];
+		$email2 = $_POST['email2'];
+		
+		
+		if($user['email']==$email2)
+		{
+
+			if ($current_user ="" ) 
+			{
+				echo " name changed";
+			}
+          
+		else{
+				echo "Invalid Input";
+			}
+	}
+?>
+		
